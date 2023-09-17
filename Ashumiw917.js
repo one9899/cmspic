ed7c81e87b65fb91ce3c9150c7f9845b40a0b6ed
one9899 <118535707+one9899@users.noreply.github.com>
2023-09-17 11:51:17 +0800

    Upload by PicGo

---
+++ Ashumiw917.js
@@ -0,0 +1,85 @@
+
+
+
+
+function tui19() {
+	
+document.writeln("<style type=\'text/css\'>.inimgx img{ position: relative; display: inline; float: left; width: 960px; height:90px; } .inimgx { position: relative; display: inline; float: left; width: 960px; height:90px; margin-bottom:5px;} .inimgx:before { content: \'广告\'; display: block; width: 60px; height: 30px; background: #999999; font-size: 14px; text-align: center; bottom: 4px; border-radius: 7px; color: #fff; opacity: 0.6; position: absolute; right: 0; z-index: 2; line-height: 30px; }</style>");
+
+document.writeln("<div style=\'width:960px;text-align:center;margin:0 auto;\'>");
+
+
+document.writeln("<div class='inimgx'><a href=\'http://\' target=\'_blank\' title=\'\'><img src=\'\' alt=\'\'\'></a></div>");
+
+
+
+
+
+
+document.writeln("<p style='margin-bottom:5px;'><a href=\'\' target=\'_blank\' title=\'\'><img src=\'\' alt=\'\' style=\'width:100%;height:0px;\'></a></p>");
+
+
+document.writeln("</div> ");
+ }
+ 
+ 
+ 
+ 
+ 
+
+function dtui19() {
+	
+document.writeln("<style type=\'text/css\'>.inimgx img{ position: relative; display: inline; float: left; width: 960px; height:90px; } .inimgx { position: relative; display: inline; float: left; width: 960px; height:90px; margin-bottom:5px;} .inimgx:before { content: \'广告\'; display: block; width: 60px; height: 30px; background: #999999; font-size: 14px; text-align: center; bottom: 4px; border-radius: 7px; color: #fff; opacity: 0.6; position: absolute; right: 0; z-index: 2; line-height: 30px; }</style>");
+
+document.writeln("<div style=\'width:960px;text-align:center;margin:0 auto;\'>");
+
+document.writeln("<div class='inimgx'><a href=\'\' target=\'_blank\' title=\'\'><img src=\'/abcimg/190416d.jpg\' alt=\'\'\'></a></div>");
+
+
+document.writeln("<p style='margin-bottom:5px;'><a href=\'tencent://message/?uin=&Site=&menu=yes\' target=\'_blank\'><img src=\'/abcimg/tui1.gif\' style=\'width:100%;height:70px;\'></a></p>");
+
+document.writeln("</div> ");
+ } 
+ 
+ 
+ 
+
+
+
+
+ 
+  
+function feidl19() {
+	
+	
+
+document.writeln("<style type=\'text/css\'>.fixe1div{position:fixed;top:30px;z-index:9999;width:120px}*html.fixe1div{position:absolute;top:expression(eval(document.documentElement.scrollTop));margin:140px 0 0 0}.fixe1div-l{left:0px}.fixe1div-r{right:0px}</style><div id=\'bar11190_big\'class=\'fixe1div fixe1div-l\'><div class=\'btnx\'style=\'height:25px;background:#CCCCCC;text-align:right;line-height:25px;width: 118px;overflow:hidden;text-align: center;\'><a href=\'javascript:void(0);\'onClick=\'bar11190_hidden()\'><span style=\'color:green;font-weight:bold;\'>广告</span>关闭</a></div><a href=\'/\'rel=\'nofollow\'target=\'_blank\'><img width=\'120\'height=\'300\'src=\'/abcimg/dlggw.png\'alt=\'\'/></a></div><div id=\'bar111901_big\'class=\'fixe1div fixe1div-r\'><div class=\'btnx\'style=\'height:25px;background:#CCCCCC;text-align:right;line-height:25px;width: 118px;overflow:hidden;text-align: center;\'><a href=\'javascript:void(0);\'onClick=\'bar111901_hidden()\'><span style=\'color:green;font-weight:bold;\'>广告</span>关闭</a></div><a href=\'/\'rel=\'nofollow\'target=\'_blank\'><img width=\'120\'height=\'300\'src=\'/abcimg/dlggw.png\'alt=\'\'/></a></div><script type=\'text/javascript\'>function bar11190_show(){document.getElementById(\'bar11190_big\').style.display=\'\';document.getElementById(\'bar11190_small\').style.display=\'none\'}function bar11190_hidden(){document.getElementById(\'bar11190_big\').style.display=\'none\';document.getElementById(\'bar11190_small\').style.display=\'\'}var autohide1190=setTimeout(\'bar11190_hidden()\',5000000);function bar111901_show(){document.getElementById(\'bar111901_big\').style.display=\'\';document.getElementById(\'bar111901_small\').style.display=\'none\'}function bar111901_hidden(){document.getElementById(\'bar111901_big\').style.display=\'none\';document.getElementById(\'bar111901_small\').style.display=\'\'}var autohide11901=setTimeout(\'bar111901_hidden()\',5000000);</script>");	
+	
+//190715d	
+	
+	
+	
+document.writeln("<style type=\'text/css\'>.fixediv{position:fixed;top:370px;z-index:9999;width:120px}*html.fixediv{position:absolute;top:expression(eval(document.documentElement.scrollTop));margin:340px 0 0 0}.fixediv-l{left:0px}.fixediv-r{right:0px}</style><div id=\'Bar1190_big\'class=\'fixediv fixediv-l\'><div class=\'btnx\'style=\'height:25px;background:#CCCCCC;text-align:right;line-height:25px;width: 118px;overflow:hidden;text-align: center;\'><a href=\'javascript:void(0);\'onClick=\'bar1190_hidden()\'><span style=\'color:green;font-weight:bold;\'>广告</span>关闭</a></div><a href=\'/\'rel=\'nofollow\'target=\'_blank\'><img width=\'120\'height=\'300\'src=\'/abcimg/dlggw.png\'alt=\'\'/></a></div><div id=\'Bar11901_big\'class=\'fixediv fixediv-r\'><div class=\'btnx\'style=\'height:25px;background:#CCCCCC;text-align:right;line-height:25px;width: 118px;overflow:hidden;text-align: center;\'><a href=\'javascript:void(0);\'onClick=\'bar11901_hidden()\'><span style=\'color:green;font-weight:bold;\'>广告</span>关闭</a></div><a href=\'/\'rel=\'nofollow\'target=\'_blank\'><img width=\'120\'height=\'300\'src=\'/abcimg/dlggw.png\'alt=\'\'/></a></div><script type=\'text/javascript\'>function bar1190_show(){document.getElementById(\'Bar1190_big\').style.display=\'\';document.getElementById(\'Bar1190_small\').style.display=\'none\'}function bar1190_hidden(){document.getElementById(\'Bar1190_big\').style.display=\'none\';document.getElementById(\'Bar1190_small\').style.display=\'\'}var autohide1190=setTimeout(\'bar1190_hidden()\',5000000);function bar11901_show(){document.getElementById(\'Bar11901_big\').style.display=\'\';document.getElementById(\'Bar11901_small\').style.display=\'none\'}function bar11901_hidden(){document.getElementById(\'Bar11901_big\').style.display=\'none\';document.getElementById(\'Bar11901_small\').style.display=\'\'}var autohide11901=setTimeout(\'bar11901_hidden()\',5000000);</script>");
+
+ }
+ 
+ 
+ 
+ 
+ function feidd19() { 
+
+
+
+
+document.writeln("<style type=\'text/css\'>#qgg_footer_float_ad{position:fixed;bottom:0;background:rgba(36,160,240,0.36);width:100%;height:150px;line-height:60px;z-index:999}.footer_float_ad_content{position:absolute;margin:auto;height:150px}.footer_float_ad_div{float:left;font-size:24px;color:#FFF;text-align:center;line-height:60px;width:98%}.footer_float_ad_div a{color:#FFF}.footer_float_ad_div a img{display:block;margin:0 auto;vertical-align:middle;width:1020px;}.footer_float_ad_button{height:60px;width:32%;float:right}.footer_float_ad_button_register{float:right;height:36px;width:32%;margin:12px 9%;background:rgba(36,160,240,0.8);text-align:center;line-height:2px;font-size:16px;font-weight:bold;border:0px solid#FFF;border-radius:9px}.footer_float_ad_button_login{float:right;height:36px;width:32%;margin:12px 9%;background:rgba(80,180,80,0.8);text-align:center;line-height:2px;font-size:16px;font-weight:bold;border:0px solid#FFF;border-radius:9px}.qgg_footer_float_ad_close{position:relative;width:2%;height:60px;color:#888;float:right;font-size:36px;text-align:center;line-height:60px}@media(max-width:800px){#qgg_footer_float_ad{display:none}}</style>");
+
+document.writeln("<div id=\'qgg_footer_float_ad\' ><div class=\'footer_float_ad_div\'><a href=\'tencent://message/?uin=208776788&Site=&menu=yes\'><img src=\'/abcimg/tui1.gif\' alt=\'广告位出租\' height=70px></a></div><span class=\'qgg_footer_float_ad_close\'>&times;</span><div class=\'footer_float_ad_div\' style='margin-bottom: -80px;margin-top:8px;'><a href=\'tencent://message/?uin=88&Site=&menu=yes\'><img src=\'/abcimg/tui1.gif\' alt=\'广告位出租\' height=70px></a></div></div>");
+
+
+
+
+document.writeln("<script>var qgg_footer_float=document.getElementById(\'qgg_footer_float_ad\');var qgg_footer_float_close=document.querySelector(\'.qgg_footer_float_ad_close\');qgg_footer_float_close.onclick=function(){qgg_footer_float.style.display=\'none\'}</script>");
+ 
+  
+ 
+  }
